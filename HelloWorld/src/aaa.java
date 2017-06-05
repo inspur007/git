c28c906ca41557b2dd3ff549cb1da9bf4c2a3d7a
inspur007 <545057287@qq.com>
2017-06-05 10:08:17 +0800

    去恶趣味

---
+++ HelloWorld/src/aaa.java
@@ -0,0 +1,10 @@
+
+public class aaa {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        for (String str : args) {
+            System.out.println(str);
+        }
+        System.out.println(11);    
+    }
+}
